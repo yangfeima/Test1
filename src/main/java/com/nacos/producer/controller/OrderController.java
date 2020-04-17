9250a3b9b491dd0beb381e712798fbd2dd0dcd67
yangmeina <728971564@qq.com>
2020-04-17 17:08:41 +0800

    项目更改

--- src/main/java/com/nacos/producer/controller/OrderController.java
+++ src/main/java/com/nacos/producer/controller/OrderController.java
@@ -26,7 +26,7 @@ public class OrderController {
         orderTabService.addOrderTab();
         //本地修改数据库更改完成
         //日志
-        System.out.println(RootContext.getXID());
+        System.out.println(RootContext.getXID());//输出结果
        // int i=1/0;
     }
 }
