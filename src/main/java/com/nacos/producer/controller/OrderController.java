6d4dd4998d6ba7b68e2639cfee577be1201810d6
yangmeina <728971564@qq.com>
2020-04-17 17:41:42 +0800

    测试分支更改

--- src/main/java/com/nacos/producer/controller/OrderController.java
+++ src/main/java/com/nacos/producer/controller/OrderController.java
@@ -23,6 +23,7 @@ public class OrderController {
     @GetMapping("/order")
     public void order(){
         //本地修改数据库更改
+        //测试合并冲突
         orderTabService.addOrderTab();
         System.out.println(RootContext.getXID());
     }
